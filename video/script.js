c4add11db969f842901af51cd24c474f6c832d26
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2020-02-16 17:59:02 +0530

    Qoulomb latest

---
+++ video/script.js
@@ -0,0 +1,56 @@
+/*
+This is a super simple slider using Ken Wheeler's "Slick Slider." 
+It's responsive, swipable and light weight.
+
+Visit Ken's site for the source code and docs. 
+
+Source: https://kenwheeler.github.io/slick/
+
+*/
+
+
+/*
+The following executes Ken's Slick Slider with several options.
+*/
+
+$('.quotes').slick({
+  dots: true,
+  infinite: true,
+  autoplay: false,
+  speed: 800,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+  draggable: false,
+  fade: true,
+});
+
+
+/*
+To prevent the flashing of unstyled content (FOUC) I created a class ".no-fouc"
+which hides the slider.  When everything is ready to roll, I simply remove the
+.no-fouc class from the slider section using the script below.  I placed the CSS snippet in the head of the HTML
+page so that it's loaded before other things for obvious reasons.  What about folks with JS turned off?  
+Well, I don't worry about them too much anymore.  Oh my.  I feel the heat from the flames already.  :) 
+*/
+
+$( document ).ready(function() {
+$('.no-fouc').removeClass('no-fouc');
+});
+
+
+// on click of the play button show the video
+$('.play-button').click( function() {
+  $('.wistia_responsive_wrapper').removeClass('playing');
+  $(this).next().addClass('playing');
+  $('.slick-dots')
+});
+
+$('.slick-dots').click( function() {
+  $('.wistia_responsive_wrapper').removeClass('playing');
+});
+
+/*
+$('.slick-dots').click( function() {
+  $(this).next().css('visibility', 'inherit');
+});
+*/
